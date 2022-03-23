3e4b29205d06ac7107d5f722a58914d1c6aada6f
y6821552 <438863580@qq.com>
2022-03-23 09:57:08 +0800

    Update simple.vue

--- example/components/simple.vue
+++ example/components/simple.vue
@@ -1,27 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-2">
-      <div class="form-group">
-        <div
-          class="btn-group-vertical buttons"
-          role="group"
-          aria-label="Basic example"
-        >
-          <button class="btn btn-secondary" @click="add">Add</button>
-          <button class="btn btn-secondary" @click="replace">Replace</button>
-        </div>
-
-        <div class="form-check">
-          <input
-            id="disabled"
-            type="checkbox"
-            v-model="enabled"
-            class="form-check-input"
-          />
-          <label class="form-check-label" for="disabled">DnD enabled</label>
-        </div>
-      </div>
-    </div>
+   
 
     <div class="col-6">
       <h3>Draggable {{ draggingInfo }}</h3>
@@ -76,12 +55,6 @@ export default {
     }
   },
   methods: {
-    add: function() {
-      this.list.push({ name: "Juan " + id, id: id++ });
-    },
-    replace: function() {
-      this.list = [{ name: "Edgard", id: id++ }];
-    },
     checkMove: function(e) {
       window.console.log("Future index: " + e.draggedContext.futureIndex);
     }
